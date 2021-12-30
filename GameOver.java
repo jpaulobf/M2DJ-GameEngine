934daab47a5ef3f4a7ab3bbc8b6e826d400f27d9
jpaulobf <jpaulobf@gmail.com>
2021-12-30 11:01:54 -0300

    implementing gameover

---
+++ GameOver.java
@@ -0,0 +1,15 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.BasicStroke;
+import java.awt.image.VolatileImage;
+import java.awt.GraphicsEnvironment;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+
+/*
+    WTCD: This class represents the gameover scene
+*/
+public class GameOver {
+    
+}
