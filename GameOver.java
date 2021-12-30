21c12c650125716be6cf7be177355ba295daff9b
jpaulobf <jpaulobf@gmail.com>
2021-12-30 11:06:08 -0300

    implementing gameover

--- GameOver.java
+++ GameOver.java
@@ -1,6 +1,5 @@
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.BasicStroke;
 import java.awt.image.VolatileImage;
 import java.awt.GraphicsEnvironment;
 import java.awt.image.BufferedImage;
