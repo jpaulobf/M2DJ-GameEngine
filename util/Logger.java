65d7e4d4fa1b4997d42ff97d03e8caf06ae9ac45
jpaulobf <jpaulobf@gmail.com>
2022-01-07 11:02:26 -0300

    adjustment

--- util/Logger.java
+++ util/Logger.java
@@ -3,19 +3,19 @@ package util;
 public class Logger {
 
     public static void WARNING(String message, Object object) {
-        System.out.println("WARNING - " + d() + " - " + message + " - " + object.getClass().getName() + " - line: " +  " - " + Thread.currentThread().getStackTrace()[2].getLineNumber());
+        System.out.println("WARNING - " + d() + " - " + message + " - " + object.getClass().getName() + " - line: " +  Thread.currentThread().getStackTrace()[2].getLineNumber());
     }
 
     public static void ERROR(String message, Object object) {
-        System.out.println("ERROR - " + d() + " - " + message + " - " + object.getClass().getName() + " - line: " +  " - " + Thread.currentThread().getStackTrace()[2].getLineNumber());
+        System.out.println("ERROR - " + d() + " - " + message + " - " + object.getClass().getName() + " - line: " + Thread.currentThread().getStackTrace()[2].getLineNumber());
     }
 
     public static void DEBUG(String message, Object object) {
-        System.out.println("DEBUG - " + d() + " - " + message + " - " + object.getClass().getName() + " - line: " +  " - " + Thread.currentThread().getStackTrace()[2].getLineNumber());
+        System.out.println("DEBUG - " + d() + " - " + message + " - " + object.getClass().getName() + " - line: " + Thread.currentThread().getStackTrace()[2].getLineNumber());
     }
 
     public static void INFO(String message, Object object) {
-        System.out.println("INFO - " + d() + " - " + message + " - " + object.getClass().getName() + " - line: " +  " - " + Thread.currentThread().getStackTrace()[2].getLineNumber());
+        System.out.println("INFO - " + d() + " - " + message + " - " + object.getClass().getName() + " - line: " +  Thread.currentThread().getStackTrace()[2].getLineNumber());
     }
 
     public static String d() {
