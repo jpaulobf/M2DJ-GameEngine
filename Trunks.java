42261f997295f71275dad006fe81d81d7dfa52f0
jpaulobf <jpaulobf@gmail.com>
2022-01-08 16:13:23 -0300

    adding trunks to the game

---
+++ Trunks.java
@@ -0,0 +1,12 @@
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import interfaces.Directions;
+import interfaces.Lanes;
+import interfaces.Sprite;
+import interfaces.SpriteCollection;
+import interfaces.Stages;
+
+public class Trunks {
+    
+}
