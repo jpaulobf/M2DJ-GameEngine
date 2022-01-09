0a5d77c8fe8447cd9d330891cb53a296762b86df
jpaulobf <jpaulobf@gmail.com>
2022-01-09 11:54:24 -0300

    Trunks implementation (WIP)

--- TreeTrunk.java
+++ TreeTrunk.java
@@ -1,10 +1,33 @@
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+/**
+ * Tree trunk (individual) class
+ */
 public class TreeTrunk  extends SpriteImpl {
+
+    //define the g2d and the trunks-images
+    private BufferedImage trunksTiles   = null;
+
+    /**
+     * constructor
+     * @param g2d
+     */
+    public TreeTrunk(Graphics2D g2d) {
+        this.g2d = g2d;
+        //Get the already loaded image from loader
+        this.trunksTiles   = (BufferedImage)LoadingStuffs.getInstance().getStuff("trunksTiles");
+    }
 
     @Override
     public void draw(long frametime) {
+        //draw the selected image
+        direction = (direction == LEFT)?0:direction;
+        this.g2d.drawImage(this.trunksTiles, this.positionX, this.positionY, this.positionX + 0, this.positionY + this.height, //dest w1, h1, w2, h2
+                                             0, 0, 0, this.height, //source w1, h1, w2, h2
+                                             null);
     }
 
     @Override
-    public void update(long frametime) {
-    }
+    public void update(long frametime) {}
 }
--- Trunks.java
+++ Trunks.java
@@ -1,23 +1,91 @@
 import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-
-import interfaces.Directions;
 import interfaces.Lanes;
 import interfaces.Sprite;
 import interfaces.SpriteCollection;
 import interfaces.Stages;
 
-public class Trunks {
+public class Trunks extends SpriteCollection {
 
     //The tile image, and its elements (positions)
-    protected int windowWidth               = 0;
-    protected int windowHeight              = 0;
-    protected Graphics2D g2d                = null;
+    protected int windowWidth       = 0;
+    protected int windowHeight      = 0;
+    protected Graphics2D g2d        = null;
 
-    //vehicles tiles
-    private BufferedImage trunksTiles      = null;
-
+    //define the vehicules array
+    private TreeTrunk [] trunks     =  new TreeTrunk[8];
     
+    /**
+     * Load the tile image
+     * @param g2d
+     */
+    public Trunks(Graphics2D g2d, int windowWidth, int windowHeight) {
+        this.g2d            = g2d;
+        this.windowWidth    = windowWidth;
+        this.windowHeight   = windowHeight;
+        for (byte i = 0; i < trunks.length; i++) {
+            trunks[i] = new TreeTrunk(this.g2d);
+        }
+    }
 
-    
+    /**
+     * Updates the elements on the screen
+     */
+    @Override
+    public void update(long frametime) {
+
+        double step     = 0d;
+        double calcPos  = 0d;
+        byte direction  = 0;
+        double position = 0;
+        short velocity  = 0;
+        byte index      = 0;
+
+        for (int i = 0; i < Stages.stg1.length; i++) {
+            for (int j = 0; j < Stages.stg1[i].length; j++) {
+
+                direction   = (byte)Stages.stg1[i][j][1];
+                position    = Stages.stg1[i][j][2];
+                velocity    = (short)Stages.stg1[i][j][3];
+                step        = (double)velocity / (double)(1_000_000D / (double)frametime);
+                calcPos     = position + (step * direction);
+
+                if (direction == RIGHT) {
+                    if (calcPos > (this.windowWidth * 1000) + Vehicle.largerVehicule) {
+                        calcPos = 0 - Vehicle.largerVehicule;
+                    }
+                } else {
+                    if (calcPos < -Vehicle.largerVehicule) {
+                        calcPos = (this.windowWidth * 1000);
+                    }
+                }
+                //atualiza a posição do objeto na array
+                Stages.stg1[i][j][2]        = (int)Math.round(calcPos);
+
+                //recupera e atualiza cada veículo
+                trunks[index].type          = (byte)Stages.stg1[i][j][0];
+                trunks[index].direction     = direction;
+                trunks[index].positionX     = (short)(position/1000);
+                //trunks[index].width         = (byte)Vehicle.vehiclesW[vehicles[index].type];
+                trunks[index++].positionY   = (short)Lanes.streetLanes[i]; //incrementa o index ao final
+            }
+        }
+    }
+
+    /**
+     * Draw the graphical vehicles elements
+     */
+    @Override
+    public void draw(long frametime) {
+        int index = 0;
+        for (byte i = 0; i < Stages.stg1.length; i++) {
+            for (byte j = 0; j < Stages.stg1[i].length; j++) {
+                trunks[index++].draw(frametime);
+            }
+        }
+    }
+
+    @Override
+    protected Sprite[] getSpriteCollection() {
+        return (this.trunks);
+    }
 }
