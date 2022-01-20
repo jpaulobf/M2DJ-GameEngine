cde4d87af7617968cee3470466f27d9d875ab2ea
jpaulobf <jpaulobf@gmail.com>
2022-01-20 13:19:01 -0300

    docker control

--- Dockers.java
+++ Dockers.java
@@ -7,24 +7,45 @@ import java.awt.Graphics2D;
  */
 public class Dockers extends SpriteCollection {
 
-    private Docker[] dockers    = new Docker[5];
-    protected Graphics2D g2d    = null;
+    private Docker[] dockers            = new Docker[5];
+    protected Graphics2D g2d            = null;
+    private boolean [] isInDock         = new boolean[dockers.length];
 
     /**
      * Dockers constructor
      */
     public Dockers(Graphics2D g2d) {
-
-        double [] positionX = {72, 348, 624, 900, 1176};
-        double [] positionY = {14, 14, 14, 14, 14};
-        short  [] width     = {96, 96, 96, 96, 96};
-        byte   [] height    = {50, 50, 50, 50, 50};
-        this.g2d            = g2d;
+        this.g2d              = g2d;
+        double [] positionX   = {72, 348, 624, 900, 1176};
+        double [] positionY   = {14, 14, 14, 14, 14};
+        short  [] width       = {66, 66, 66, 66, 66};
+        byte   [] height      = {50, 50, 50, 50, 50};
 
         for (int i = 0; i < dockers.length; i++) {
             dockers[i] = new Docker(this.g2d);
             dockers[i].config(positionX[i], positionY[i], width[i], height[i]);
         }
+
+        //initialize the dockers
+        for (int i = 0; i < this.isInDock.length; i++) {
+            isInDock[i] = false;
+        }
+    }
+
+    /**
+     * Getter
+     * @return
+     */
+    public boolean [] getIsInDock() {
+        return (this.isInDock);
+    }
+
+    /**
+     * Setter
+     * @param position
+     */
+    public void setIsInDock(int position) {
+        this.isInDock[position] = true;
     }
 
     @Override
