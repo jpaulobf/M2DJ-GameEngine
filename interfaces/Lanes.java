0b58382f08b7f8c0a39228f9d63454f35a54c513
jpaulobf <jpaulobf@gmail.com>
2022-01-25 10:14:35 -0300

    river lane adjustement

--- interfaces/Lanes.java
+++ interfaces/Lanes.java
@@ -7,7 +7,9 @@ public interface Lanes {
  
     public static final int docksLanes[] = { 14, 64 };
 
-    public static final int riverLanes[] = { 80, 144, 208, 272, 336 };
+    //public static final int riverLanes[] = { 80, 144, 208, 272, 336 };
+    public static final int riverLanes[] = { 64, 128, 192, 256, 320 };
+    
     //for the lane background
     public static final int streetLanes[] = { 461, 525, 589, 653, 716 };
 }
