f488f4993c18358d213b4ab3e8630b63f3c856f3
jpaulobf <jpaulobf@gmail.com>
2022-02-01 23:37:02 -0300

    HUD with frogger lives

--- HUD.java
+++ HUD.java
@@ -1,29 +1,35 @@
-import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.BasicStroke;
 import java.awt.image.VolatileImage;
 import java.awt.image.BufferedImage;
 import java.awt.GraphicsEnvironment;
-import java.awt.Transparency;
+import java.awt.Color;
 
 public class HUD {
 
     private Graphics2D g2d              = null;
     private Scenario scenarioRef        = null;
     private Frog frogRef                = null;
-    private VolatileImage bgBufferImage = null;
-    private int windowWidth             = 0;
-    private int windowHeight            = 0;
-    private int lives                   = 0;
+    private VolatileImage hudBG         = null;
+    private BufferedImage livesTile     = null;
+    private Graphics2D bg2d             = null;
+    private short windowWidth           = 0;
+    private short windowHeight          = 0;
+    private byte lives                  = 0;
+    private byte separator              = 8;
+    private byte initialDistance        = 10;
+    private byte HUDHeight              = 0;
     
-    public HUD(Graphics2D g2d, int HUDHeight, Scenario scenario, Frog frog) {
+    public HUD(Graphics2D g2d, byte HUDHeight, Scenario scenario, Frog frog) {
         this.g2d            = g2d;
         this.scenarioRef    = scenario;
         this.frogRef        = frog;
-        this.windowWidth    = scenarioRef.getWindowWidth();
-        this.windowHeight   = scenarioRef.getWindowHeight();
+        this.windowWidth    = (short)scenarioRef.getWindowWidth();
+        this.windowHeight   = (short)scenarioRef.getWindowHeight();
+        this.HUDHeight      = HUDHeight;
         this.lives          = this.frogRef.getLives();
-        this.bgBufferImage  = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration().createCompatibleVolatileImage(this.windowWidth, this.windowHeight);
+        this.hudBG          = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration().createCompatibleVolatileImage(this.windowWidth, HUDHeight);
+        this.livesTile      = (BufferedImage)LoadingStuffs.getInstance().getStuff("live");
+        this.bg2d           = (Graphics2D)this.hudBG.getGraphics();
     }
 
     /**
@@ -39,8 +45,19 @@ public class HUD {
      * @param frametime
      */
     public void draw(long frametime) {
+
+        this.bg2d.setBackground(Color.BLACK);
+        this.bg2d.clearRect(0, 0, this.windowWidth, this.windowHeight);
+
+        for (int i = 0; i < this.lives; i++) {
+            this.bg2d.drawImage(this.livesTile, initialDistance + ((i * separator) + i * this.livesTile.getWidth()), 0, initialDistance + ((i * this.livesTile.getWidth()) + this.livesTile.getWidth() + (i * separator)), this.livesTile.getHeight(), //dest w1, h1, w2, h2
+                                                 0, 0, this.livesTile.getWidth(), this.livesTile.getHeight(),  //source w1, h1, w2, h2
+                                                 null);
+        }
         //After construct the bg once, copy it to the graphic device
-        this.g2d.drawImage(this.bgBufferImage, 0, 0, null);
+        this.g2d.drawImage(this.hudBG, 0, 0, this.windowWidth, this.HUDHeight,  //dest w1, h1, w2, h2
+                                       0, 0, this.hudBG.getWidth(), this.hudBG.getHeight(),           //source w1, h1, w2, h2
+                                       null);
     }
 
     /**
