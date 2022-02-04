07e2cbba914f8da00c665e3d404f9041efba9485
jpaulobf <jpaulobf@gmail.com>
2022-02-04 09:31:07 -0300

    New HUD coding

--- HUD.java
+++ HUD.java
@@ -7,29 +7,37 @@ import java.awt.Color;
 public class HUD {
 
     private Graphics2D g2d              = null;
-    private Scenario scenarioRef        = null;
     private Frog frogRef                = null;
     private VolatileImage hudBG         = null;
     private BufferedImage livesTile     = null;
     private Graphics2D bg2d             = null;
-    private short windowWidth           = 0;
-    private short windowHeight          = 0;
     private byte lives                  = 0;
-    private byte separator              = 8;
-    private byte initialDistance        = 10;
+    private int wwm                     = 0;
+    private int whm                     = 0;
     private byte HUDHeight              = 0;
+    private final byte separator        = 8;
+    private final byte initialDistance  = 10;
+    private final byte initialHeight    = 8;
     
-    public HUD(Graphics2D g2d, byte HUDHeight, Scenario scenario, Frog frog) {
-        this.g2d            = g2d;
-        this.scenarioRef    = scenario;
-        this.frogRef        = frog;
-        this.windowWidth    = (short)scenarioRef.getWindowWidth();
-        this.windowHeight   = (short)scenarioRef.getWindowHeight();
+    /**
+     * HUD Constructor
+     * @param g2d
+     * @param wwm
+     * @param whm
+     * @param HUDHeight
+     * @param scenario
+     * @param frog
+     */
+    public HUD(Graphics2D g2d, int wwm, int whm, byte HUDHeight, Frog frog) {
         this.HUDHeight      = HUDHeight;
+        this.wwm            = wwm;
+        this.whm            = whm;
+        this.g2d            = g2d;
+        this.frogRef        = frog;
         this.lives          = this.frogRef.getLives();
-        this.hudBG          = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration().createCompatibleVolatileImage(this.windowWidth, HUDHeight);
-        this.livesTile      = (BufferedImage)LoadingStuffs.getInstance().getStuff("live");
+        this.hudBG          = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration().createCompatibleVolatileImage(wwm, HUDHeight);
         this.bg2d           = (Graphics2D)this.hudBG.getGraphics();
+        this.livesTile      = (BufferedImage)LoadingStuffs.getInstance().getStuff("live");
     }
 
     /**
@@ -46,17 +54,23 @@ public class HUD {
      */
     public void draw(long frametime) {
 
+        //clear the backbuffer
         this.bg2d.setBackground(Color.BLACK);
-        this.bg2d.clearRect(0, 0, this.windowWidth, this.windowHeight);
+        this.bg2d.clearRect(0, 0, wwm, HUDHeight);
 
+        //render the HUD
         for (int i = 0; i < this.lives; i++) {
-            this.bg2d.drawImage(this.livesTile, initialDistance + ((i * separator) + i * this.livesTile.getWidth()), 0, initialDistance + ((i * this.livesTile.getWidth()) + this.livesTile.getWidth() + (i * separator)), this.livesTile.getHeight(), //dest w1, h1, w2, h2
-                                                 0, 0, this.livesTile.getWidth(), this.livesTile.getHeight(),  //source w1, h1, w2, h2
-                                                 null);
+            this.bg2d.drawImage(this.livesTile, initialDistance + ((i * separator) + (i * this.livesTile.getWidth())), 
+                                                initialHeight, 
+                                                initialDistance + ((i * separator) + (i * this.livesTile.getWidth()) + this.livesTile.getWidth()), 
+                                                initialHeight + this.livesTile.getHeight(), //dest w1, h1, w2, h2
+                                                0, 0, this.livesTile.getWidth(), this.livesTile.getHeight(),  //source w1, h1, w2, h2
+                                                null);
         }
-        //After construct the bg once, copy it to the graphic device
-        this.g2d.drawImage(this.hudBG, 0, 0, this.windowWidth, this.HUDHeight,  //dest w1, h1, w2, h2
-                                       0, 0, this.hudBG.getWidth(), this.hudBG.getHeight(),           //source w1, h1, w2, h2
+
+        //After HUD rendered, copy to G2D
+        this.g2d.drawImage(this.hudBG, 0, this.whm, this.wwm, (this.whm + this.HUDHeight),   //dest w1, h1, w2, h2
+                                       0, 0, this.hudBG.getWidth(), this.hudBG.getHeight(),  //source w1, h1, w2, h2
                                        null);
     }
 
@@ -64,6 +78,5 @@ public class HUD {
      * 
      */
     public void reset() {
-
     }
 }
