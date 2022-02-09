464dfdde1cc3c0b0894c5a7be64036398dfc16d8
jpaulobf <jpaulobf@gmail.com>
2022-02-09 08:37:15 -0300

    audio class improvements

--- util/Audio.java
+++ util/Audio.java
@@ -7,11 +7,20 @@ import javax.sound.sampled.Clip;
   
 public class Audio {
   
+    private static final byte STOP              = 0;
+    private static final byte PAUSE             = 1;
+    private static final byte PLAY              = 100;
     private Clip clip                           = null;
-    private String status                       = null;
+    private byte status                         = STOP;
     private AudioInputStream audioInputStream   = null;
     private boolean ready                       = false;
+    private long microsecondPosition            = 0;
   
+    /**
+     * Audio class
+     * @param filePath
+     * @param loop
+     */
     public Audio(String filePath, int loop) {
         try {
             audioInputStream = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
@@ -41,21 +50,35 @@ public class Audio {
         if (clip.isRunning()) {
             clip.stop();
         }
-        clip.setMicrosecondPosition(0);
+        clip.setMicrosecondPosition(this.microsecondPosition);
         try {
             Thread.sleep(1);    
         } catch (Exception e) {
         }
         clip.loop(loop);
         clip.start();
-        status = "play";
+        status = PLAY;
+    }
+
+    public void pause() {
+        this.microsecondPosition = clip.getMicrosecondPosition();
+        clip.stop();
+        status = PAUSE;
+    }
+
+    public void stop() {
+        clip.stop();
+        this.microsecondPosition = 0;
+        clip.setMicrosecondPosition(this.microsecondPosition);
+        clip.setFramePosition(0);
+        status = STOP;
     }
 
     public boolean isReady() {
         return (this.ready);
     }
 
-    public String getStatus() {
+    public byte getStatus() {
         return (this.status);
     }
 }
