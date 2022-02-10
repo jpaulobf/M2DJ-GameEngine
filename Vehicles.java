f70908f12742604c39f6d7032fdb80c40a05f380
jpaulobf <jpaulobf@gmail.com>
2022-02-10 15:52:39 -0300

    vehicle now pause

--- Vehicles.java
+++ Vehicles.java
@@ -16,6 +16,7 @@ public class Vehicles extends SpriteCollection {
 
     //define the vehicules array
     private Vehicle [] vehicles     =  new Vehicle[Stages.CURRENT_STAGE_CARS[Stages.CURRENT_STAGE]];
+    private volatile boolean stopped    = false; 
 
     /**
      * Load the tile image
@@ -43,36 +44,38 @@ public class Vehicles extends SpriteCollection {
         short velocity  = 0;
         byte index      = 0;
 
-        for (int i = 0; i < Stages.S1_CARS.length; i++) {
-            
-            direction            = (byte)Stages.S1_CARS[i][0][0];
-            velocity             = (short)Stages.S1_CARS[i][1][0];
+        if (!this.stopped) {
+            for (int i = 0; i < Stages.S1_CARS.length; i++) {
 
-            for (int j = 0; j < Stages.S1_CARS[i][3].length; j++) {
+                direction   = (byte)Stages.S1_CARS[i][0][0];
+                velocity    = (short)Stages.S1_CARS[i][1][0];
 
-                position             = Stages.S1_CARS[i][3][j];
-                step                 = (double)velocity / (double)(1_000_000D / (double)frametime);
-                calcPos              = position + (step * direction);
-                vehicles[index].type = (byte)Stages.S1_CARS[i][2][0];
+                for (int j = 0; j < Stages.S1_CARS[i][3].length; j++) {
 
-                if (direction == RIGHT) {
-                    if (calcPos > (this.windowWidth * 1000) + Vehicle.largerVehicule) {
-                        calcPos = 0 - Vehicle.largerVehicule;
+                    position             = Stages.S1_CARS[i][3][j];
+                    step                 = (double)velocity / (double)(1_000_000D / (double)frametime);
+                    calcPos              = position + (step * direction);
+                    vehicles[index].type = (byte)Stages.S1_CARS[i][2][0];
+
+                    if (direction == RIGHT) {
+                        if (calcPos > (this.windowWidth * 1000) + Vehicle.largerVehicule) {
+                            calcPos = 0 - Vehicle.largerVehicule;
+                        }
+                    } else {
+                        if (calcPos < -Vehicle.largerVehicule) {
+                            calcPos = (this.windowWidth * 1000);
+                        }
                     }
-                } else {
-                    if (calcPos < -Vehicle.largerVehicule) {
-                        calcPos = (this.windowWidth * 1000);
-                    }
+                    //atualiza a posição do objeto na array
+                    Stages.S1_CARS[i][3][j]      = (int)Math.round(calcPos);
+
+                    //recupera e atualiza cada veículo
+                    vehicles[index].direction    = direction;
+                    vehicles[index].positionX    = (short)(position/1000);
+                    //incrementa o index ao final
+                    vehicles[index].width        = (byte)Vehicle.vehiclesW[vehicles[index].type];
+                    vehicles[index++].positionY  = (short)Lanes.streetLanes[i];
                 }
-                //atualiza a posição do objeto na array
-                Stages.S1_CARS[i][3][j]      = (int)Math.round(calcPos);
-
-                //recupera e atualiza cada veículo
-                vehicles[index].direction    = direction;
-                vehicles[index].positionX    = (short)(position/1000);
-                //incrementa o index ao final
-                vehicles[index].width        = (byte)Vehicle.vehiclesW[vehicles[index].type];
-                vehicles[index++].positionY  = (short)Lanes.streetLanes[i];
             }
         }
     }
@@ -93,5 +96,12 @@ public class Vehicles extends SpriteCollection {
     @Override
     protected Sprite[] getSpriteCollection() {
         return (this.vehicles);
+    }
+
+    /**
+     * Toogle the stop control
+     */
+    public void toogleStop() {
+        this.stopped = !this.stopped;
     }
 }
