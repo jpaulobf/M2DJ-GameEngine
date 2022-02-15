4f13bac356e6451eb4532e09a0a199cc47b79f3d
jpaulobf <jpaulobf@gmail.com>
2022-02-15 11:16:06 -0300

    opengl turned off

--- SplashScreen.java
+++ SplashScreen.java
@@ -186,8 +186,8 @@ public class SplashScreen extends JFrame implements Runnable {
     */
     public static void main(String[] args) throws Exception {
         //enable openGL
-        System.setProperty("sun.java2d.opengl", "True");
-        //System.setProperty("sun.java2d.d3d", "False");
+        //System.setProperty("sun.java2d.opengl", "True");
+        //System.setProperty("sun.java2d.d3d", "True");
 
         //start the thread
         Thread thread = new Thread(new SplashScreen(), "engine");
