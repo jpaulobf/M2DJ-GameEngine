0c42eb16462c0c92bf54467460a7e7ea1d123d78
jpaulobf <jpaulobf@gmail.com>
2022-02-19 14:55:56 -0300

    Message animations (wip)

--- Message.java
+++ Message.java
@@ -1,13 +1,13 @@
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import util.Audio;
+import java.awt.Color;
 
 /**
  * Class to show messages on screen
  */
 @SuppressWarnings("unused")
 public class Message {
-    private Graphics2D g2d                          = null;
     private BufferedImage stageTile                 = null;
     private BufferedImage goTile                    = null;
     private BufferedImage messageTile               = null;
@@ -18,22 +18,22 @@ public class Message {
     private volatile boolean showing                = false;
     private volatile boolean showStage              = false;
     private volatile long framecounter              = 0;
-    private volatile long animationDuration         = 4_000_000_000L;
+    private volatile long animationDuration         = 3_500_000_000L;
     private volatile long quarterAnimationDuration  = animationDuration / 4;
     private volatile long thirthAnimationDuration   = quarterAnimationDuration * 3;
     private volatile Audio beepstart                = null;
 
-    private volatile int w1 = 0;
-    private volatile int h1 = 0;
-    private volatile int w2 = 0;
-    private volatile int h2 = 0;
-    private volatile double startOgW1 = 0D;
-    private volatile int ogW1 = 0;
-    private volatile int ogW2 = 0;
-    private volatile int ogH1 = 0;
-    private volatile int ogH2 = 0;
-    private volatile double diff = 0;
-    private volatile double steps = 0D;
+    private volatile int w1                         = 0;
+    private volatile int h1                         = 0;
+    private volatile int w2                         = 0;
+    private volatile int h2                         = 0;
+    private volatile double startOgW1               = 0D;
+    private volatile int ogW1                       = 0;
+    private volatile int ogW2                       = 0;
+    private volatile int ogH1                       = 0;
+    private volatile int ogH2                       = 0;
+    private volatile double diff                    = 0;
+    private volatile double steps                   = 0D;
 
     /**
      * HUD Constructor
@@ -44,10 +44,9 @@ public class Message {
      * @param scenario
      * @param frog
      */
-    public Message(Graphics2D g2d, int wwm, int whm, Game game) {
+    public Message(Game game, int wwm, int whm) {
         this.wwm            = wwm;
         this.whm            = whm;
-        this.g2d            = g2d;
         this.gameReference  = game;
         this.messageTile    = (BufferedImage)LoadingStuffs.getInstance().getStuff("stage-clear");
         this.stageTile      = (BufferedImage)LoadingStuffs.getInstance().getStuff("stage-1");
@@ -60,7 +59,6 @@ public class Message {
      * @param frametime
      */
     public void update(long frametime) {
-
         if (this.showStage) {
             this.framecounter += frametime;
             if (this.framecounter > thirthAnimationDuration) {
@@ -77,16 +75,21 @@ public class Message {
                     this.beepstart.play();
                 }
             }
+
+            //calculate the animation steps
+            this.steps = diff * frametime;
+            this.startOgW1 += this.steps;
+            this.w1 = (int)this.startOgW1;
+            this.w2 = this.w1 + this.temp.getWidth();
+            this.h1 = ogH1;
+            this.h2 = ogH2;
         } else {
             this.temp = messageTile;
+            this.w1 = (int)((wwm - this.temp.getWidth()) / 2);
+            this.w2 = this.w1 + this.temp.getWidth();
+            this.h1 = (int)((whm - this.temp.getHeight()) / 2) - 20;
+            this.h2 = this.ogH1 + this.temp.getHeight();
         }
-
-        this.steps = diff * frametime;
-        this.startOgW1 += this.steps;
-        this.w1 = (int)this.startOgW1;
-        this.w2 = this.w1 + this.temp.getWidth();
-        this.h1 = ogH1;
-        this.h2 = ogH2;
     }
 
     /**
@@ -98,7 +101,9 @@ public class Message {
         this.ogW2       = this.ogW1 + this.temp.getWidth();
         this.ogH1       = (int)((whm - this.temp.getHeight()) / 2) - 20;
         this.ogH2       = this.ogH1 + this.temp.getHeight();
-        this.diff       = (ogW1 - startOgW1) / (quarterAnimationDuration * times);
+        if (times > 0) {
+            this.diff = (ogW1 - startOgW1) / (quarterAnimationDuration * times);
+        }
     }
 
     /**
@@ -107,9 +112,10 @@ public class Message {
      */
     public void draw(long frametime) {
         if (this.showing) {
-            this.g2d.drawImage(this.temp, w1, h1, w2, h2, //dest w1, h1, w2, h2
-                                          0, 0, this.temp.getWidth(), this.temp.getHeight(),  //source w1, h1, w2, h2
-                                          null);
+            //draw
+            this.gameReference.getG2D().drawImage(this.temp, w1, h1, w2, h2, //dest w1, h1, w2, h2
+                                                             0, 0, this.temp.getWidth(), this.temp.getHeight(),  //source w1, h1, w2, h2
+                                                             null);
         }
     }
 
@@ -150,7 +156,12 @@ public class Message {
      * Show stage message
      */
     public void showStageAnnouncement() {
-        this.showing = true;
-        this.showStage = true;
+        this.showing    = true;
+        this.showStage  = true;
+    }
+
+    public void showStates() {
+        System.out.println(this.showing);
+        System.out.println(this.showStage);
     }
 }
