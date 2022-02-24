ffd71aff7f861357206cfec34f9fbd558b50ff6e
jpaulobf <jpaulobf@gmail.com>
2022-02-24 16:01:18 -0300

    new GameFactory

--- GameFactory.java
+++ GameFactory.java
@@ -1,12 +1,11 @@
 import interfaces.IGame;
-import java.awt.Graphics2D;
 import java.awt.Dimension;
 
 /**
  * Game factory
  */
 public class GameFactory {
-    public static IGame getGameInstance(Graphics2D g2d, Dimension size) {
-        return (new Game(g2d, size));
+    public static IGame getGameInstance() {
+        return (new Game());
     }
 }
