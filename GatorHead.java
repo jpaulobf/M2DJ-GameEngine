7bd51e93559c95b2c99d16989de79bd67a8cfc0f
jpaulobf <jpaulobf@gmail.com>
2022-03-02 21:27:07 -0300

    New Gator control logic

--- GatorHead.java
+++ GatorHead.java
@@ -1,20 +1,21 @@
 import java.awt.Graphics2D;
 import interfaces.SpriteCollection;
 import java.awt.image.BufferedImage;
+import interfaces.Stages;
 
 /**
  * Class represents the gator head
  */
 public class GatorHead extends SpriteImpl {
 
-    private BufferedImage gatorSprite       = null;
-    private SpriteCollection spriteColRef   = null;
-    private Dockers dockers                 = null;
-    private final double [] positionsX      = {71, 347, 623, 899, 1175};
-    private volatile boolean sorting        = true;
-    private volatile long framecounter      = 0;
-    private volatile boolean isVisible      = false;
-    private volatile byte sorted            = -1;
+    private BufferedImage gatorSprite               = null;
+    private volatile SpriteCollection spriteColRef  = null;
+    private volatile Dockers dockers                = null;
+    private final double [] positionsX              = {71, 347, 623, 899, 1175};
+    private volatile boolean finished               = false;
+    private volatile long framecounter              = 0;
+    private volatile boolean isVisible              = false;
+    private volatile byte sorted                    = -1;
 
     /**
      * Constructor
@@ -29,37 +30,49 @@ public class GatorHead extends SpriteImpl {
     }
 
     @Override
-    public void update(long frametime) {
-        if (this.sorting) {
+    public synchronized void update(long frametime) {
+        //increment framecounter
+        this.framecounter += frametime;
+
+        //just one per cycle
+        if (this.framecounter == frametime) {
+            //start the process
+            this.finished = false;
+
             if ((!dockers.getDockersComplete()) && (dockers.getFreeDockersCounter() > 1)) {
                 //recover the taked dockers
                 boolean [] isInWhichDock = dockers.getIsInDock();
 
                 //sort one free docker
-                //TODO: TEST AGAINST MOSQUITO
                 do {
                     sorted = (byte)(Math.random() * 5);
-                } while (isInWhichDock[sorted] != false);
+                } while (isInWhichDock[sorted] != false || sorted == this.dockers.getCurrentMosquito());
+
+                //after sort, set the gator head
+                this.dockers.setCurrentGatorHead(sorted);
                 
                 //update the X & Y position
                 this.positionX = this.positionsX[sorted];
-
-                //set animating true
-                this.sorting = false;
             }
         } else {
-            //set visible
-            this.isVisible = true;
-            this.framecounter += frametime;
+            if (sorted != -1) {
+                //set visible
+                this.isVisible = true;
 
-            if (this.framecounter >= 5_000_000_000L) {
-                this.setInvisible();
+                //duration
+                if (this.framecounter >= (Stages.GATOR_HEAD_CONFIG[Stages.CURRENT_STAGE][1] * 1_000_000_000L)) {
+                    this.setInvisible();
+                    this.dockers.setCurrentGatorHead((byte)-1);
+                    this.finished = true;
+                }
+            } else {
+                this.finished = true;
             }
         }
     }
 
     @Override
-    public void draw(long frametime) {
+    public synchronized void draw(long frametime) {
         if (this.isVisible) {
             Graphics2D g2d = this.spriteColRef.getG2D();
             g2d.drawImage(this.gatorSprite, (int)this.positionX, (int)this.positionY + this.scenarioOffsetY, (int)(this.positionX + this.width), (int)(this.positionY + this.height + this.scenarioOffsetY), //dest w1, h1, w2, h2
@@ -69,28 +82,28 @@ public class GatorHead extends SpriteImpl {
     }
 
     /**
+     * Verify if the appearence (in this cycle) has finished
+     * @return
+     */
+    public boolean appearenceFinished() {
+        return (this.finished);
+    }
+
+    /**
      * Makes the mosquito invisible
      */
-    public void setInvisible() {
-        this.sorting        = true;
+    public synchronized void setInvisible() {
         this.framecounter   = 0;
         this.isVisible      = false;
         this.positionX      = -1000;
         this.sorted         = -1;
-    }
-
-    /**
-     * Return if gator is animating (?)
-     * @return
-     */
-    public boolean isSorting() {
-        return (this.sorting);
+        this.dockers.setCurrentGatorHead((byte)-1);
     }
 
     /**
      * Verify if the gator is in the Docker
      */
-    public boolean isInTheDocker(int docker) {
+    public synchronized boolean isInTheDocker(int docker) {
         return (this.sorted == docker);
     }
 
@@ -100,5 +113,4 @@ public class GatorHead extends SpriteImpl {
     public void reset() {
         this.setInvisible();
     }
-
 }
