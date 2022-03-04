b356297be812ef4004b683f2d09463bd8058733a
jpaulobf <jpaulobf@gmail.com>
2022-03-04 13:05:42 -0300

    trunk snake (WIP)

--- TrunkSnake.java
+++ TrunkSnake.java
@@ -7,48 +7,82 @@ import interfaces.Stages;
  */
 public class TrunkSnake extends Snake {
 
+    private volatile double additionalStep  = 0;
+    private volatile boolean enabled        = false;
+
+    //getter
+    public boolean isEnabled()  {   return (this.enabled);  }
+
     /**
      * Class constructor
      */
     public TrunkSnake(IGame game, int windowWidth) {
         super(game, windowWidth);
-        this.positionX      = 100;
-        this.positionY      = Lanes.riverLanes[3] - 60;
-        this.direction      = RIGHT;
-        this.velocity       = Stages.SNAKE_SPEED[Stages.CURRENT_STAGE][0];
-        this.calcPosition   = this.positionX;
-        this.visible        = false;
+        if (Stages.TRUNKS[Stages.CURRENT_STAGE].length != 0 && 
+            Stages.TRUNKS[Stages.CURRENT_STAGE][2].length != 0 && 
+            Stages.TRUNKS[Stages.CURRENT_STAGE][3].length != 0) {
+            this.positionX      = Stages.TRUNKS[Stages.CURRENT_STAGE][2][3][0];
+            this.positionY      = Lanes.riverLanes[3] - 60;
+            this.velocity       = Stages.SNAKE_SPEED[Stages.CURRENT_STAGE][1];
+            this.calcPosition   = positionX;
+            this.direction      = RIGHT;
+            this.visible        = true;
+            this.enabled        = true;
+        } else {
+            this.visible        = false;
+            this.positionX      = -1_000;
+        }
+        if (this.velocity == -1) {
+            this.visible        = false;
+            this.positionX      = -1_000;
+        }
     }
 
     @Override
     public void update(long frametime) {
-        if (Stages.SNAKE_SPEED[Stages.CURRENT_STAGE][0] > -1) {
-            
+        if (this.enabled) {
             this.framecounter += frametime;
-            if (this.framecounter >= 0 && this.framecounter < 150_000_000) {
-                this.positionXSource = 0;
-            } else if (this.framecounter >= 150_000_000 && this.framecounter < 300_000_000) {
-                this.positionXSource = this.width;
-            } else if (this.framecounter >= 300_000_000 && this.framecounter < 450_000_000) {
-                this.positionXSource = 2 * this.width;
-            } else if (this.framecounter >= 450_000_000 && this.framecounter < 600_000_000) {
-                this.positionXSource = 3 * this.width;
-            } else if (this.framecounter >= 600_000_000 && this.framecounter < 750_000_000) {
-                this.positionXSource = 4 * this.width;
-            } else {
+            if (this.framecounter >= 300_000_000) {
+                this.positionXFrame = (byte)((this.positionXFrame + 1)%4);
                 this.framecounter = 0;
             }
-           
+            //calc the tile frame
+            this.positionXSource = this.positionXFrame * this.width;
+        
             //calc step times direction
-            this.calcPosition += ((double)velocity / (double)(1_000_000D / (double)frametime) * direction);
+            this.calcPosition += (((double)velocity / (double)(1_000_000D / (double)frametime) * this.direction) + (this.additionalStep));
             this.positionX = (short)(this.calcPosition/1000);
 
         } else {
-            this.positionX = -1000;
+            this.positionX = -1_000;
             this.visible = false;
         }
     }
 
-    
+    /**
+     * Trunk snake reset method
+     */
+    public void reset() {
+        this.positionX      = -50;
+        this.direction      = RIGHT;
+        this.calcPosition   = positionX * 1_000;
+    }
 
+    /**
+     * Increment the velocity using the trunk velocity
+     */
+    public void setAditionalStep(double additionalStep) {
+        this.additionalStep = additionalStep;
+    }
+
+    /**
+     * Change snake direction
+     */
+    public void toogleDirection() {
+        if (this.direction == RIGHT) {
+            this.direction = LEFT;
+        } else {
+            this.direction = RIGHT;
+        }
+    }
 }
